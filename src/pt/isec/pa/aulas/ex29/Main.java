74037a9e7e97a43a121e639903cfc8a6f40d1573
EdEquinox <zemarquez97@hotmail.com>
2023-05-11 15:44:08 +0100

    aula18

---
+++ src/pt/isec/pa/aulas/ex29/Main.java
@@ -0,0 +1,11 @@
+package pt.isec.pa.aulas.ex29;
+
+import javafx.application.Application;
+import pt.isec.pa.aulas.ex29.ui.gui.MainJFX;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Base Exerc 29");
+        Application.launch(MainJFX.class,args);
+    }
+}
